5fb5b512297850138cfd0124bfb19237ffbcf8bc
stanoevboris <stanoevboris98@gmail.com>
2023-08-05 19:25:26 +0200

    implemented custom classification methods

--- classification.py
+++ classification.py
@@ -1,10 +1,48 @@
+import numpy as np
+
 from neural import *  # DRMs
 from learning import *  # starspace
 
 from sklearn.metrics import roc_auc_score
+from scipy import sparse
+from tqdm import tqdm
+
+
+def examine_batch_predictions(test_features_indexes,
+                              unique_classes,
+                              predictions,
+                              predictions_scores):
+    stats = {}
+    for index in tqdm(test_features_indexes):
+        stats.setdefault(index, {})
+        stats[index] = {label: 0 for label in unique_classes}
+        stats[index]["pred_score"] = []
+    for index, pred_value, pred_score in tqdm(zip(test_features_indexes, predictions, predictions_scores)):
+        stats[index][pred_value] += 1
+        stats[index]["pred_score"].append(pred_score)
+
+    batch_preds_classes = []
+    batch_preds_scores = []
+    for key in stats:
+        pred = max(stats[key], key=lambda k: stats[key].get(k) if k in unique_classes else -1)
+        pred_score = np.average(stats[key]["pred_score"])
+        batch_preds_classes.append(pred)
+        batch_preds_scores.append(pred_score)
+
+    return batch_preds_classes, batch_preds_scores
+
+
+def encode_classes(train_classes, test_classes):
+    encoder = preprocessing.LabelEncoder()
+    encoder.fit(train_classes)
+    train_classes = encoder.transform(train_classes)
+    test_classes = encoder.transform(test_classes)
+    return train_classes, test_classes, encoder
 
 
 def prop_drm_classification(args, train_features, train_classes, test_features, test_classes):
+    train_classes, test_classes, _ = encode_classes(train_classes, test_classes)
+
     model = E2EDNN(num_epochs=args.epochs,
                    learning_rate=args.learning_rate,
                    hidden_layer_size=args.hidden_size,
@@ -29,6 +67,8 @@ def prop_drm_classification(args, train_features, train_classes, test_features, 
 
 
 def prop_star_classification(args, train_features, train_classes, test_features, test_classes):
+    train_classes, test_classes, _ = encode_classes(train_classes, test_classes)
+
     model = starspaceLearner(epoch=args.epochs,
                              learning_rate=args.learning_rate,
                              neg_search_limit=args.negative_search_limit,
@@ -38,7 +78,7 @@ def prop_star_classification(args, train_features, train_classes, test_features,
     # standard fit predict
     model.fit(train_features, train_classes)
     predictions = model.predict(test_features,
-                          clean_tmp=False)
+                                clean_tmp=False)
 
     if len(predictions) == 0:
         perf_roc = 0
@@ -59,6 +99,63 @@ def prop_star_classification(args, train_features, train_classes, test_features,
         if len(np.unique(test_classes)) == 2:
             roc = roc_auc_score(
                 test_classes, preds_scores)
+            logging.info(roc)
+        else:
+            roc = 0
+    except Exception as es:
+        print(es)
+        return
+
+    return acc, roc
+
+
+def prop_drm_woe_classification(args, train_features, train_classes, test_features, test_classes):
+    print('test')
+    pass
+
+
+def prop_star_woe_classification(args, train_features, train_classes, test_features, test_classes):
+    train_x = sparse.csr_matrix(train_features)
+    train_y = [train_classes[index] for index in train_features.index]
+    test_x = sparse.csr_matrix(test_features)
+    test_y = [test_classes[index] for index in test_features.index]
+
+    train_y, test_y, encoder = encode_classes(train_y, test_y)
+
+    unique_classes = set(test_y)
+    test_classes_encoded = encoder.transform(list(test_classes.values()))
+
+    model = starspaceLearner(epoch=args.epochs,
+                             learning_rate=args.learning_rate,
+                             neg_search_limit=args.negative_search_limit,
+                             dim=args.hidden_size,
+                             max_neg_samples=args.negative_samples_limit)
+
+    # standard fit predict
+    model.fit(train_x, train_y)
+    try:
+        predictions = model.predict(test_x,
+                                    clean_tmp=False)
+
+        preds_scores = model.predict(
+            test_x,
+            clean_tmp=True,
+            return_int_predictions=False,
+            return_scores=True)  # use scores for auc.
+
+        batch_preds_classes, batch_preds_scores = examine_batch_predictions(test_features.index,
+                                                                            unique_classes, predictions, preds_scores)
+        if len(batch_preds_classes) == 0:
+            roc = 0
+            acc = 0
+            return acc, roc
+
+        acc = accuracy_score(test_classes_encoded, batch_preds_classes)
+        logging.info(acc)
+
+        if len(np.unique(test_classes_encoded)) == 2:
+            roc = roc_auc_score(
+                test_classes_encoded, batch_preds_scores)
             logging.info(roc)
         else:
             roc = 0

6c1d609c9473d9e23f25dfbd85c02daba47ed419
stanoevboris <stanoevboris98@gmail.com>
2023-08-07 22:17:29 +0200

    added new arguments related to the traditional classifiers and updated dictionary with funcs

--- benchmark.py
+++ benchmark.py
@@ -1,3 +1,5 @@
+import argparse
+
 import numpy as np
 
 import logging
@@ -7,24 +9,38 @@ from sklearn.metrics import roc_auc_score
 from sklearn import preprocessing
 
 from learning import preprocess_and_split
-from propositionalization import table_generator, generate_relational_words
-from classification import prop_drm_classification, prop_star_classification
+from propositionalization import table_generator, generate_relational_words, generate_custom_relational_words
+from classification import *
 
 logging.basicConfig(format='%(asctime)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S')
 logging.getLogger().setLevel(logging.INFO)
 
+feature_func = {"woe": generate_custom_relational_words,
+                "sklearn_tfidf": generate_relational_words}
+
 classifier_func = {
-    "DRM": prop_drm_classification,
-    "starspace": prop_star_classification
+    "DRM": {'sklearn_tfidf': prop_drm_tfidf_classification,
+            'woe': prop_drm_woe_classification},
+    "starspace": {'sklearn_tfidf': prop_star_tfidf_classification,
+                  'woe': prop_star_woe_classification},
+    "svm_learner": {
+        'sklearn_tfidf': traditional_learner_tfidf_classification
+    },
+    "extra_tree_learner": {
+        'sklearn_tfidf': traditional_learner_tfidf_classification
+    },
+    "random_forest_learner": {
+        'sklearn_tfidf': traditional_learner_tfidf_classification
+    },
+    "ada_boost_learner": {'sklearn_tfidf': traditional_learner_tfidf_classification},
+    "gradient_boost_learner": {'sklearn_tfidf': traditional_learner_tfidf_classification}
 }
 
 if __name__ == "__main__":
 
-    import argparse
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("--learner", default="starspace")
+    parser.add_argument("--learner", default="gradient_boost_learner")
     parser.add_argument("--learning_rate",
                         default=0.001,
                         type=float,
@@ -56,8 +72,31 @@ if __name__ == "__main__":
         help=
         "Negative search limit (see starspace docs for extensive description)")
     parser.add_argument(
+        "--n_estimators",
+        default=16,
+        type=int,
+        help=
+        "No. of estimators used mainly for traditional classifiers")
+    parser.add_argument(
+        "--kernel",
+        default='linear',
+        type=str,
+        help=
+        "Kernel used for SVC")
+    parser.add_argument(
+        "--C",
+        default=1,
+        type=int,
+        help=
+        "Regularization parameter for SVC")
+    parser.add_argument(
+        "--gamma",
+        default='scale',
+        help=
+        "Kernel coefficient for kernel with value ‘rbf’, used in SVC")
+    parser.add_argument(
         "--representation_type",
-        default="woe",
+        default="sklearn_tfidf",
         type=str,
         help=
         "Type of representation and weighting. tfidf or binary, also supports scikit's implementations (ordered "
@@ -73,7 +112,11 @@ if __name__ == "__main__":
                       args.negative_samples_limit, args.hidden_size,
                       args.negative_search_limit, args.representation_type,
                       args.num_features],
-        "default": []
+        "svm_learner": [args.epochs, args.representation_type, args.kernel, args.C, args.gamma],
+        "extra_tree_learner": [args.epochs, args.representation_type, args.n_estimators],
+        "random_forest_learner": [args.epochs, args.representation_type, args.n_estimators],
+        "ada_boost_learner": [args.epochs, args.representation_type, args.n_estimators],
+        "gradient_boost_learner": [args.epochs, args.representation_type, args.n_estimators, args.learning_rate]
     }
 
     variable_types_file = open(
@@ -123,7 +166,8 @@ if __name__ == "__main__":
                                               token_pattern='[a-zA-Z0-9$&+,:;=?@#|<>.^*()%!-_]+',
                                               ngram_range=(1, 1))  # higher relation orders result in high
                     # memory load, thread with caution!
-                    train_features, train_classes, vectorizer = generate_relational_words(
+                    generate_relational_words_func = feature_func[args.representation_type]
+                    train_features, train_classes, vectorizer = generate_relational_words_func(
                         tables,
                         fkg,
                         target_table,
@@ -133,24 +177,19 @@ if __name__ == "__main__":
                         encoder=encoder,
                         vectorization_type=args.representation_type,
                         num_features=args.num_features)
-                    test_features, test_classes = generate_relational_words(
+                    test_features, test_classes = generate_relational_words_func(
                         tables,
                         fkg,
                         target_table,
                         target_attribute,
-                        relation_order=(1, 2),
+                        relation_order=(1, 1),
                         vectorizer=vectorizer,
                         indices=test_index,
                         encoder=encoder,
                         vectorization_type=args.representation_type,
                         num_features=args.num_features)
 
-                    le = preprocessing.LabelEncoder()
-                    le.fit(train_classes.values)
-                    train_classes = le.transform(train_classes)
-                    test_classes = le.transform(test_classes)
-
-                    classify_func = classifier_func[args.learner]
+                    classify_func = classifier_func[args.learner][args.representation_type]
                     acc, auc_roc = classify_func(args=args,
                                                  train_features=train_features,
                                                  train_classes=train_classes,

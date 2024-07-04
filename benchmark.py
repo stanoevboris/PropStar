3d3c1b1461fea02bcc33e3f452057cb93ab7c203
stanoevboris <stanoevboris98@gmail.com>
2024-07-04 23:27:17 +0200

    adopted benchmark script to the latest changes

--- benchmark.py
+++ benchmark.py
@@ -1,17 +1,15 @@
 import logging
 import argparse
 import time
-from concurrent.futures import ThreadPoolExecutor
-from threading import Lock
 
-from rdm.dataset_processor import DatasetProcessor, DatasetConfig
-from utils import load_yaml_config
+from rdm.dataset_processor import DatasetProcessor
+from rdm.utils import load_yaml_config
 
 
 class Benchmark:
     def __init__(self, args):
         self.args = args
-        self.config_file = 'datasets.yaml'
+        self.config_file = 'rdm/datasets.yaml'
         self.initialize_logging()
 
     @staticmethod
@@ -21,11 +19,11 @@ class Benchmark:
             datefmt='%m-%d %H:%M:%S',
             level=logging.INFO)
 
-    def load_datasets(self):
+    def load_datasets(self, problem_type: str):
         # Load datasets configuration from YAML or similar
         config = load_yaml_config(self.config_file)
 
-        for dataset in config['datasets']:
+        for dataset in config['datasets'][problem_type]:
             if dataset.get('enabled', True):
                 yield dataset
 
@@ -33,31 +31,44 @@ class Benchmark:
         if self.args.prop_methods is None:
             raise ValueError('No propositional methods specified')
 
+    def check_problem_types(self):
+        if self.args.problem_types is None:
+            raise ValueError('No problem types specified')
+
     def run(self):
         self.check_prop_methods()
-        for dataset_info in self.load_datasets():
-            processor = DatasetProcessor(dataset_info, self.args)
-            processor.process()
+        for problem_type in self.args.problem_types:
+            for dataset_info in self.load_datasets(problem_type=problem_type):
+                processor = DatasetProcessor(dataset_info, self.args, problem_type)
+                processor.process()
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--results_file", default="results/experiments_21_05_2024.csv", help="Path to the results file")
-    parser.add_argument("--classifier_config", default="classifier_config.yaml",
+    parser.add_argument("--results_file", default="results/debugging_new_datasets_23_06_2024.csv",
+                        help="Path to the results file")
+    parser.add_argument("--classifier_config", default="rdm/classifier_config.yaml",
                         help="Path to the classifiers dataset_config file")
-    parser.add_argument("--fe_config", default="fe_config.yaml",
+    parser.add_argument("--fe_config", default="rdm/fe_config.yaml",
                         help="Path to the feature engineering transformers config file")
     parser.add_argument('--folds', type=int, default=10, help='Number of folds for cross-validation')
     parser.add_argument(
-        "--prop_methods", nargs='*', default=['wordification'],
+        "--prop_methods", nargs='*', default=['denormalization'],
         choices=["wordification", "denormalization"])
+    parser.add_argument(
+        "--problem_types", nargs='*', default=['binary_classification', 'multiclass_classification'],
+        choices=["binary_classification", "multiclass_classification"])
     return parser.parse_args()
 
 
 def main():
+    start_time = time.time()
     args = parse_arguments()
     benchmark = Benchmark(args)
     benchmark.run()
+
+    end_time = time.time()
+    logging.info(f"Experiments total execution time: {end_time - start_time:.4f} seconds")
 
 
 if __name__ == "__main__":
